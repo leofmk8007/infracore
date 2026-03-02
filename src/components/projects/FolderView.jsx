d19e95d8f0fecc7cea2ab795cb7fb2faf25b3670
Lokan Máquinas e Equipamentos <marketinglokan@gmail.com>
2026-03-02 18:58:00 +0000

    File changes

--- src/components/projects/FolderView.jsx
+++ src/components/projects/FolderView.jsx
@@ -190,6 +190,12 @@ function SubProjectRow({ subProject, tasks, clientId, onDeleteSub, onEditSub }) 
           )}
         </div>
       )}
+
+      <TaskDetailModal
+        task={detailTask}
+        open={!!detailTask}
+        onClose={() => setDetailTask(null)}
+      />
     </div>
   );
 }
